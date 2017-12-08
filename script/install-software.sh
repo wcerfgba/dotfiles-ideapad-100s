a15c6e70e5ddedd22bd5be69081ae8a796808b04
John L. Preston <wcerfgba@riseup.net>
2017-12-08 19:03:38 +0000

    Fix status_command and add pasystray and redshift

--- script/install-software.sh
+++ script/install-software.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ATOM_URL=`curl -w "%{url_effective}\n" -I -L -s -S https://atom.io/download/rpm -o /dev/null`
-sudo dnf install i3 conky emacs-nox zsh $ATOM_URL
+sudo dnf install i3 conky pasystray paprefs pavumeter pavucontrol paman redshift-gtk emacs-nox zsh $ATOM_URL
 chsh -s /usr/bin/zsh
 zsh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.6/install.sh | zsh
